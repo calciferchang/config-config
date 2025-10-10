a8700256a7f46ced7c6b958659fb468071fc6008
calcifer chang <c@lcifer.com>
2025-10-10 19:20:15 -0400

    add ssh config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 ln -sf ~/config-config/zshrc ~/.zshrc
 ln -sf ~/config-config/gitconfig ~/.gitconfig
+ln -sf ~/config-config/ssh/config ~/.ssh/config
 ln -sf ~/config-config/wezterm.lua ~/.wezterm.lua
 ln -sf ~/config-config/config/helix ~/.config/helix
 ln -sf ~/config-config/config/yazi ~/.config/yazi
