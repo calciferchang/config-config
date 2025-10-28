6e64284f9ec908376af4caea95f18a90cc497de9
calcifer chang <c@lcifer.com>
2025-10-27 20:59:29 -0400

    add zed config

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ ln -sf ~/config-config/gitconfig ~/.gitconfig
 ln -sf ~/config-config/ssh/config ~/.ssh/config
 ln -sf ~/config-config/config/helix ~/.config/helix
 ln -sf ~/config-config/config/yazi ~/.config/yazi
+ln -sf ~/config-config/config/zed ~/.config/zed
 ln -sf ~/config-config/config/nvim ~/.config/nvim
